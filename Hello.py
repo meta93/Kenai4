dd6d21f7e5f220ee24771f636adffbf1d991eb3b
Abdul-Baaki <baakiyakubu87@gmail.com>
2022-04-21 16:12:42 +0000

    changes in hello

--- Hello.py
+++ Hello.py
@@ -3,3 +3,28 @@ print("Hello World!")
 a = 32
 b = 5
 print(a - b)
+
+MyFavouriteFoods = ['yam', 'banku', 'kenkey']
+# print(MyFavouriteFoods)
+
+MyFavouriteFoods.append('Fufu')
+
+# print(MyFavouriteFoods)
+MyFavouriteFoods.insert(1, 'Gari')
+# print(MyFavouriteFoods)
+
+#iteration using the 'for in'
+for item in MyFavouriteFoods:
+    print(item)
+
+# print(len(MyFavouriteFoods))
+
+# list of numbers
+Mynumbers = [2, 34, 56, 89, 0, 1, 35, 67]
+Mynumbers.sort()
+print(Mynumbers)
+
+fruitsTuple = ('apple', 'mangoes', 'papaya', 'pawpaw', 'pineapple')
+print(fruitsTuple)
+# for i in Mynumbers:
+    
