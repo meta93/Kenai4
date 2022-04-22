8b0b93c043e78145d88c17cc9d58faf51f6d40bb
RMWachira <robertwachira@gmail.com>
2022-04-22 17:52:21 +0300

    step1

---
+++ first.py
@@ -0,0 +1,18 @@
+studentname = input('what is the student''s name? ')
+score = int(input)('what is their score? ')
+grade = ''
+if score >=80:
+    grade = 'A'
+elif score >= 70 and score < 80:
+    grade = 'B'
+elif score >= 60 and score < 70:
+    grade = 'C'
+elif score >= 50 and score < 60:
+    grade = 'D'
+else:
+    grade = 'E'
+print(studentname + ' has a grade of ' + grade)
+
+
+
+
